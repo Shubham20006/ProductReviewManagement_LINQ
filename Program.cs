7e2aecb783f52df35b8f829846986d669b05ad0e
Shubham20006 <shubhamchaudhari1506@gmail.com>
2023-02-04 13:23:52 +0530

    Welcome

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO THE 'PRODUCT REVIEW MANAGEMENT BY USING LINQ' PROGRAM.");
+        }
+    }
+}
