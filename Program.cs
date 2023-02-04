667b4eacaa4f8f0b9dd82db5cfa799550b0bf67e
Shubham20006 <shubhamchaudhari1506@gmail.com>
2023-02-04 13:27:48 +0530

    Creating a class for adding fields

--- Program.cs
+++ Program.cs
@@ -12,6 +12,26 @@ namespace ProductReviewManagement
         public static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO THE 'PRODUCT REVIEW MANAGEMENT BY USING LINQ' PROGRAM.");
+
+            ProductReview PR =new ProductReview();
+            List<ProductReview> list = new List<ProductReview>()
+            {
+                new ProductReview(){ProductID=1, UserID=1, Rating=4, Review="good", isLike=true},
+                new ProductReview(){ProductID=4, UserID=2, Rating=2, Review="bad", isLike=false},
+                new ProductReview(){ProductID=3, UserID=5, Rating=5, Review="good", isLike=true},
+                new ProductReview(){ProductID=5, UserID=3, Rating=2, Review="bad", isLike=false},
+                new ProductReview(){ProductID=1, UserID=1, Rating=3, Review="nice", isLike=true},
+                new ProductReview(){ProductID=6, UserID=6, Rating=4, Review="good", isLike=true},
+                new ProductReview(){ProductID=11, UserID=2, Rating=5, Review="good", isLike=true},
+                new ProductReview(){ProductID=9, UserID=4, Rating=3, Review="nice", isLike=true},
+                new ProductReview(){ProductID=3, UserID=7, Rating=4, Review="good", isLike=true},
+                new ProductReview(){ProductID=2, UserID=10, Rating=1, Review="bad", isLike=false},
+                new ProductReview(){ProductID=5, UserID=3, Rating=5, Review="good", isLike=true},
+                new ProductReview(){ProductID=9, UserID=5, Rating=4, Review="good", isLike=true},
+                new ProductReview(){ProductID=1, UserID=1, Rating=4, Review="good", isLike=true},
+                new ProductReview(){ProductID=4, UserID=2, Rating=2, Review="bad", isLike=false},
+                new ProductReview(){ProductID=3, UserID=5, Rating=5, Review="good", isLike=true}
+            };
         }
     }
 }
